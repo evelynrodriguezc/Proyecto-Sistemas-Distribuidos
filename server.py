412983f93a5ce9c9e2cdc54dbfd4949e73ad6af7
daniel <Daniel.serna.olarte@gmail.com>
2021-11-07 14:17:51 -0500

    WIP

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import threading
 import socket
 import pickle
+import random
 
 PORT = 8001
 BUFFER_SIZE = 1024
 
-bd = {'username':[], 'socket':[]}
+bd = {'username':[], 'socket':[], 'color':[]}
+colors = ['Red', 'Yellow' , 'White', 'Green']
 
 
 class Server(threading.Thread):
@@ -15,15 +17,43 @@ class Server(threading.Thread):
         self.conn = conn
         #the address is the address bound to the socket at the other end of the connection
         self.addr = addr
+        #it tells me if the user is logged in
+        self.User_logged = False
 
+    def assign_color(self) -> str:
+        for el in colors:
+            if el not in bd['color']:
+                return el
+    
     def run(self):
         with self.conn:
+            print('conected', self.addr)
             while True:
                 #data is a dictionary
-                data = self.conn.recv(BUFFER_SIZE)
-                print(data.decode())
-                msg = f'hola {data.decode()}'
-                self.conn.sendall()
+                data = pickle.loads(self.conn.recv(BUFFER_SIZE))
+
+                if len(bd['username']) < 4:
+                    if not self.User_logged and not data['conected']:
+                        #change options
+                        self.User_logged = True
+                        data['conected'] = True
+                        data['color_user'] = self.assign_color()
+
+                        bd['username'].append(data['name_user'])
+                        data['bd_users'] = bd['username']
+                        bd['socket'].append(self.conn)
+                        bd['color'].append(data['color_user'])
+
+
+                        #send other players
+                        for s in bd['socket']:
+                            if s != self.conn:
+                                s.sendall(pickle.dumps(data))
+                        self.conn.sendall(pickle.dumps(data))
+                     
+                else:
+                    self.conn.sendall(pickle.dumps('Server is full'))
+                    break
 
 
 def create_server():
@@ -37,5 +67,4 @@ def create_server():
 
 if __name__ == '__main__':
     create_server()
-
 
